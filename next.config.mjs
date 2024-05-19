3f74d71d683e8dd7ee7c25adf710aa4eea7d08fd
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2024-05-19 14:32:34 -0300

    :sparkles: feat: add firebase img

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { domains: ['localhost'] }, 
+  images: { domains: ['localhost', 'firebasestorage.googleapis.com'] }, 
 };
 
 export default nextConfig;
